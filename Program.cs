fdff3edef0c33ee2f2e2b6d375847d88c39e8d78
SparksCool <70763188+SparksCool@users.noreply.github.com>
2024-04-15 15:30:16 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Configure URLs
+builder.WebHost.UseUrls("http://*:8080", "https://*:8081");
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
