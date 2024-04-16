f0e6e0566031606dda7933a6b8851a648ddcb4f0
SparksCool <70763188+SparksCool@users.noreply.github.com>
2024-04-16 03:13:29 -0400

    Added project view proof of concept
    Creates a project entry for every valid json file in the Resources/Projects folder

---
+++ Pages/Projects.cshtml.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+
+namespace SparktonianNet.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        class Project
+        {
+            // Define the properties of a Project
+            // Name of the project
+            public string Name { get; set; }
+            // Description of the project
+            public string Description { get; set; }
+            // URL to the GitHub repository
+            public string GitHubUrl { get; set; }
+            // URL to the demo of the project
+            public string DemoUrl { get; set; }
+            // URL to the image of the project
+            public string ImageUrl { get; set; }
+            // Tags associated with the project
+            public string[] Tags { get; set; }
+        }
+        
+        public string GetProjectsHTML()
+        {
+
+            // HTML Template string for a project entry
+            string HTMLTemplate =
+                "<div class=\"projectEntry text-dark text-decoration-none\" href=\"{3}\">" +
+                "<h3 class=\"projectTitle\">{0}</h3>" +
+                "<p class=\"projectDesc\">{1}</p>" +
+                "<ul class=\"projectTags\">{2}</ul>" +
+                "</div> <br>";
+            // HTML Template for a tag
+            string HTMLTagTemplate = "<li class=\"projectTag\">{0}</li>";
+
+            // Output formatted HTML listing all projects
+            string HTMLOutput = "";
+            // Get a list of all JSON files in the Projects folder
+            var files = System.IO.Directory.GetFiles("Resources/Projects/", "*.json");
+
+            // Loop through each JSON file in the Projects folder
+            foreach (var file in files)
+            {
+                // Read the contents of the JSON file into a Project object
+                Project? project = JsonSerializer.Deserialize<Project>(System.IO.File.ReadAllText(file));
+
+                // List of tags for a project
+                List<string> tags = new List<string>();
+
+                // If the project is null, output "NULL!" and continue to the next project
+                if (project == null)
+                {
+                    HTMLOutput += "NULL!";
+                    continue;
+                }
+
+                // Assemble the HTML output for the project
+                foreach (var tag in project.Tags)
+                {
+                    tags.Add(string.Format(HTMLTagTemplate, tag));
+                }
+
+                HTMLOutput += string.Format(HTMLTemplate, project.Name, project.Description, string.Join("", tags), project.GitHubUrl);
+
+            }
+
+            return HTMLOutput;
+        }
+    }
+}
