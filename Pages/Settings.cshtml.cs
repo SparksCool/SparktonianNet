1e2b391b5d592dabb270f96a772d9afdebf6ef3e
SparksCool <70763188+SparksCool@users.noreply.github.com>
2024-04-19 03:18:25 -0400

    Added dark mode

---
+++ Pages/Settings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SparktonianNet.Pages
+{
+    public class SettingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
