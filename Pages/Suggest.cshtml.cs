29c88554e23c3802adba5ae20b4f80d7cbf3ea15
SparksCool <70763188+SparksCool@users.noreply.github.com>
2024-04-22 01:51:23 -0400

    start suggestion system

---
+++ Pages/Suggest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SparktonianNet.Pages
+{
+    public class SuggestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/SuggestionConfirmation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SparktonianNet.Pages
+{
+    public class SuggestionConfirmationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
